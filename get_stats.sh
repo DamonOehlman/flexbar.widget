18dd201683f8d900586ab14b91b3d5a860e61848
Damon Oehlman <damon.oehlman@gmail.com>
2016-11-18 09:22:36 +1100

    getting all the stats :)

--- get_stats.sh
+++ get_stats.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
 main() {
+  echo "$(get_system_stats)|$(get_date_time)"
+}
+
+get_system_stats() {
   local battery
   local mem
+  local cpu
 
-  battery=$(pmset -g batt | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d';')
+  # retrieve system level stats
+  battery="$(pmset -g batt | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d';')"
+  mem="$(ps -A -o %mem | awk '{s+=$1} END {print s}')"
+  cpu="$(ps -A -o %cpu | awk '{s+=$1} END { printf("%.2f",s/8); }')"
 
-  mem=$(ps -A -o %mem | awk '{s+=$1} END {print s}')
+  echo "${battery}|${mem}|${cpu}";
+}
 
-  echo "${battery}|${mem}";
+get_date_time() {
+  local current_date
+  local current_time
+
+  current_date="$(date +"%a %d %b")"
+  current_time="$(date +"%H:%M")"
+
+  echo "${current_date}|${current_time}"
 }
 
 main "$@"
