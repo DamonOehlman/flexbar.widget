f72e05ac94e777bc92372b9f7c604550cb360144
Damon Oehlman <damon.oehlman@gmail.com>
2016-12-30 11:06:38 +1100

    explicitly specify kwmc path

--- get_kwm_info.sh
+++ get_kwm_info.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
+KWMC="/usr/local/bin/kwmc"
+
 main() {
   echo "$(get_workspace_details)|$(get_window_details)"
 }
 
 get_workspace_details() {
-  echo "$(kwmc query space active name)"
+  echo "$(${KWMC} query space active name)"
 }
 
 get_window_details() {
-  echo "$(kwmc query window focused name)"
+  echo "$(${KWMC} query window focused name)"
 }
 
 main "$@"
